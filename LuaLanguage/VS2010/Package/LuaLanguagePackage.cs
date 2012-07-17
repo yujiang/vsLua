fe258ab2222c25dc53895867eca33e1de4b8047f
yujiang <yu.jiang.163@gmail.com>
2012-07-17 10:54:07 +0800

    lost file LuaLaunguagePackage.cs

---
+++ LuaLanguage/VS2010/Package/LuaLanguagePackage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Package;
+
+//namespace LuaLanguagePackage
+//{
+//    internal class MyConstants
+//    {
+//        public const string languageName = "MyLanguage";
+//        public const int languageNameResourceID = 106;
+//    }
+
+//    [ProvideLanguageExtension(typeof(MyLanguageService), ".myext")]
+
+//    [ProvideLanguageService(typeof(MyLanguageService),
+//                            MyConstants.languageName,
+//                            MyConstants.languageNameResourceID,
+//        // Optional language service properties
+//        CodeSense = true,  // General IntelliSense support
+//        RequestStockColors = false, // Custom colorable items
+//        EnableASyncCompletion = true,  // supports background parsing
+//        MatchBraces = true,  // Match braces on command
+//        MatchBracesAtCaret = true   // Match braces while typing
+//                           )]
+
+//    class MyLanguagePackage
+//    {
+//    }
+//}
